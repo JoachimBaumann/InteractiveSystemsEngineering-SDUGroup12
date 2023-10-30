bb23461c1cf2588bad8f435348fa2292cae8561e
Sebastian <sechr17@student.sdu.dk>
2023-10-30 13:00:25 +0100

    fixed bugs

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from datetime import datetime
 from flask import jsonify
 from sqlalchemy.sql import func
 
-
 app = Flask(__name__)
-
+app.secret_key = "some_random_string_here"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
@@ -17,19 +16,6 @@ def hello():
     return render_template("index.html", expenses=expenses)
 
 
-@app.route("/add_expense", methods=["POST"])
-def add_expense():
-    category = request.form["category"]
-    amount = request.form["amount"]
-    # Fetch date from the form or use current date
-    date = request.form.get("date", datetime.utcnow())
-
-    expense = Expense(category=category, amount=amount, date=date)
-    db.session.add(expense)
-    db.session.commit()
-    return redirect(url_for("hello"))
-
-
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(80), nullable=False)
@@ -38,24 +24,6 @@ class Expense(db.Model):
 
     def __repr__(self):
         return f"Expense('{self.category}', '{self.amount}', '{self.date}')"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-@app.route("/add_expense", methods=["POST"])
-def add_expense():
-    category = request.form["category"]
-    amount = request.form["amount"]
-    date = request.form.get("date", datetime.utcnow())
-
-    expense = Expense(category=category, amount=amount, date=date)
-    db.session.add(expense)
-    db.session.commit()
-
-    flash("Expense added successfully!", "success")
-    return redirect(url_for("hello"))
 
 
 @app.route("/delete_expense/<int:id>", methods=["POST"])
@@ -76,27 +44,8 @@ def expense_data():
     )
     categories = [item[0] for item in data]
     amounts = [item[1] for item in data]
-    return {"categories": categories, "amounts": amounts}
 
-
-@app.route("/expense_data")
-def expense_data():
-    categories = [expense.category for expense in Expense.query.all()]
-    amounts = [expense.amount for expense in Expense.query.all()]
-
-    data = {
-        "categories": list(set(categories)),
-        "amounts": [
-            sum(
-                amount
-                for category, amount in zip(categories, amounts)
-                if category == cat
-            )
-            for cat in set(categories)
-        ],
-    }
-
-    return jsonify(data)
+    return jsonify({"categories": categories, "amounts": amounts})
 
 
 @app.route("/add_expense", methods=["POST"])
@@ -112,3 +61,7 @@ def add_expense():
 
     flash("Expense added successfully!", "success")
     return redirect(url_for("hello"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
